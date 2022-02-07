f43f28e353c1fef5fa65d41b68d8e50e83923304
BrowenSiz <BrowenSiz@yandex.ru>
2022-02-07 14:39:37 +0300

    L4

--- main.js
+++ main.js
@@ -1,18 +1,54 @@
-const fs = require('fs')
-const readline = require('readline')
+const fs = require('fs/promises');
+const { lstatSync } = require('fs');
+const inquirer = require('inquirer');
+const yargs = require('yargs');
+const path = require('path');
 
-const access = fs.createReadStream('./access.log', 'utf8')
-const requests1 = fs.createWriteStream('./requests_IP41.log')
-const requests2 = fs.createWriteStream('./requests_IP111.log')
+let executionDir = process.cwd();
 
-readline.createInterface({
-    input: access,
-    terminal: false,
-}).on('line', (line) => {
-    if (line.includes("89.123.1.41")) {
-        requests1.write(line + "\n")
+const options = yargs
+    .options('d', {
+        describe: 'Path to the directory',
+        alias: 'dir',
+        default: process.cwd(),
+    })
+    .options('s', {
+        alias: 'substring',
+        default: '',
+    }).argv;
+console.log(options);
+
+const isDir = (path) => lstatSync(path).isDirectory();
+
+const run = async () => {
+    const list = await fs.readdir(executionDir);
+    let chosenPath;
+    await inquirer.prompt([
+        {
+            name: 'fileName',
+            type: 'list',
+            message: 'Choose category: ',
+            choices: list,
+        }
+    ]).then(({ fileName }) => {
+        chosenPath = path.join(executionDir, fileName);
+    });
+
+    if (isDir(chosenPath)) {
+        executionDir = chosenPath;
+        return await run();
     }
-    else if (line.includes("34.48.240.111")) {
-        requests2.write(line + "\n")
+    else {
+        const data = await fs.readFile(chosenPath, 'utf-8');
+        if (!options.substring) {
+            console.log(data)
+        }
+        else {
+            const regExp = new RegExp(options.substring, 'igm');
+            console.log(data.match(regExp));
+        }
     }
-})
+
+}
+
+run();
